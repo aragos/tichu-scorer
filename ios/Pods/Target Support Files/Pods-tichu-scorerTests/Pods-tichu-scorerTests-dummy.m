bb1c7b333dcd92a05068c4075474aed31c298763
Peter Schmitt <aragos@Io.fios-router.home>
2019-05-05 15:06:11 -0400

    Initial ios app shell with pods

---
+++ ios/Pods/Target Support Files/Pods-tichu-scorerTests/Pods-tichu-scorerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tichu_scorerTests : NSObject
+@end
+@implementation PodsDummy_Pods_tichu_scorerTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-tichu-scorerUITests/Pods-tichu-scorerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tichu_scorerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_tichu_scorerUITests
+@end
