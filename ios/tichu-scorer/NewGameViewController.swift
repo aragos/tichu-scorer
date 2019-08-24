46be46b0a250055d4cfefd87d14773ab2c2cb4de
Peter Schmitt <aragos@gmail.com>
2019-08-24 12:57:00 -0400

    new game controller

---
+++ ios/tichu-scorer/NewGameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewGameViewController.swift
+//  tichu-scorer
+//
+//  Created by Peter Schmitt on 5/11/19.
+//  Copyright © 2019 Peter Schmitt. All rights reserved.
+//
+
+import UIKit
+
+class NewGameViewController: UIViewController {
+
+    var gameStateController: GameStateController?
+    var playerController: PlayerController?
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }    
+}
